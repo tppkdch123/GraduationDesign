e86c7a0227388ddd7a798e9df904795771cc2fde
tppkdch123 <hgftderwsy@qq.com>
2018-03-09 16:39:42 +0800

    更新

---
+++ src/main/java/org/graduationdesign/interceptor/SingleSignOnInterceptor.java
@@ -0,0 +1,32 @@
+package org.graduationdesign.interceptor;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SingleSignOnInterceptor extends AbstractController implements HandlerInterceptor{
+    @Nullable
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+
+    }
+}
--- src/test/java/test/ThreadTest.java
+++ src/test/java/test/ThreadTest.java
@@ -5,14 +5,54 @@ import org.junit.Test;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ThreadTest {
     @Test
-    public void testThreadMXBean(){
-        ThreadMXBean threadMXBean= ManagementFactory.getThreadMXBean();
-        ThreadInfo[] threadInfo=threadMXBean.dumpAllThreads(false,false);
-        for(ThreadInfo threadInfo1:threadInfo){
-            System.out.println(threadInfo1.getThreadId()+" "+threadInfo1.getThreadName());
+    public void testThreadMXBean() {
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        ThreadInfo[] threadInfo = threadMXBean.dumpAllThreads(false, false);
+        for (ThreadInfo threadInfo1 : threadInfo) {
+            System.out.println(threadInfo1.getThreadId() + " " + threadInfo1.getThreadName());
+        }
+    }
+
+    @Test
+    public void testThread() {
+        final Map<String, Integer> map = new HashMap<>();
+        map.put("ss", 0);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(() -> {
+            for (int i = 0; i < 100; i++) {
+                map.put("ss", map.get("ss") + 2);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        executorService.execute(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(map.get("ss") + "=====================================");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        executorService.execute(() -> map.put("ss", 1));
+        for (int i = 0; i < 1000; i++) {
+            try {
+                testThreadMXBean();
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
